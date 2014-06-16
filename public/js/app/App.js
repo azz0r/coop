ffbb44bf670dea61b352c8a1524b26aff7c2731f
Aaron Lote <aaron.lote@gmail.com>
2014-06-16 22:52:30 +0100

    Navigation is now a sub view that reacts to login

--- public/js/app/App.js
+++ public/js/app/App.js
@@ -1,15 +1,15 @@
-define(["marionette", "app/views/layout/sidebar", "app/views/layout/user", "jquery", "app/helper/user"],
-  function(marionette, SidebarView, UsersActionsView, $, userCheck) {
+define(["marionette", "app/views/layout/sidebar", "app/views/layout/navigation", "jquery", "app/helper/user"],
+  function(marionette, SidebarView, NavigationView, $, userCheck) {
     "use strict"
     var app;
     app = new marionette.Application()
     app.addRegions({
       sidebar: "#sidebarInner",
-      userActions: '#userActions',
+      navigation: '#navigation',
       content: "#contentInner"
     });
     app.addInitializer(function() {
-      app.userActions.show(new UsersActionsView ())
+      app.navigation.show(new NavigationView ())
       app.sidebar.show(new SidebarView())
     })
     /* if we catch a user viewing a page they shouldnt */
--- public/js/app/views/layout/navigation.js
+++ public/js/app/views/layout/navigation.js
@@ -1,4 +1,4 @@
-define(["marionette", "text!app/templates/layout/user.html"],
+define(["marionette", "text!app/templates/layout/navigation.html"],
   function(Marionette, Template) {
     "use strict";
     return Marionette.ItemView.extend({
@@ -11,12 +11,6 @@ define(["marionette", "text!app/templates/layout/user.html"],
         MyApp.vent.on('signOut:successful', function(){
           that.render()
         })
-      },
-      serializeData: function(){
-        return {
-          user: localStorage.getObject('user'),
-          signedIn: (localStorage.getObject('user') != null ? true : false)
-        };
       }
     })
   })
