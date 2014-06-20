a3d526a0b1b6548ae1373c0adb105f2415d851df
Aaron Lote <aaron.lote@gmail.com>
2014-06-20 15:10:36 +0100

    Re render the messages view when I get a message

--- public/js/app/views/messages/collection.js
+++ public/js/app/views/messages/collection.js
@@ -15,6 +15,5 @@ define([
       collection: new Collection(),
       pageCollection: new Collection()
 
-
     })
   })
--- public/js/app/views/messages/item.js
+++ public/js/app/views/messages/item.js
@@ -9,15 +9,51 @@ define(["marionette", "text!app/templates/messages/item.html", "app/models/messa
       className: 'row',
 
 
+      events: {
+        'click .sendMessage': 'onClickSendMessage'
+      },
+
+
+      onClickSendMessage: function(ev) {
+
+        // prevent default form submission
+        ev.preventDefault();
+
+        // create a new model with our messages schema
+        var newModel = new Model({
+          message: this.$el.find('.newMessage').val(),
+          to_user: this.otherUserId,
+          in_reply_to: this.lastMessage.in_reply_to.$id
+        });
+
+        //js scope
+        var that = this;
+
+        newModel.save({}, {
+          success: function() {
+            // when we save the post trigger the event that will reload the index view
+            MyApp.vent.trigger('filter:change')
+          },
+          error: function() {
+            alert('There was an error saving your reply, please try again');
+          }
+        })
+      },
+
+
       serializeData: function() {
 
-        var modelObject = this.model.toJSON();
-        var arrayObject = $.map(modelObject, function (value, key) { return value; });
+        this.modelObject = this.model.toJSON();
+        this.arrayObject = $.map(this.modelObject, function (value, key) { return value; });
+        this.lastMessage = this.arrayObject[this.arrayObject.length - 1];
+
+        this.otherUserId = this.lastMessage.to_user._id.$id = _.getUser()._id.$id
+          ? this.lastMessage.by_user._id.$id : this.lastMessage.to_user._id.$id;
 
         return {
-          messages: arrayObject,
-          lastMessage: arrayObject[arrayObject.length - 1],
-          messageCount: arrayObject.length
+          messages: this.arrayObject.reverse(),
+          lastMessage: this.lastMessage,
+          messageCount: this.arrayObject.length
         }
       }
 
